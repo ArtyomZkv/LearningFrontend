dc49111aa393b38be736512af8a5915e7bfe970b
Artyom <zujkov.ks52@gmail.com>
2024-09-30 23:33:31 +0300

    add readme.txt

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.write("<h2>Первая программа на JavaScript</h2>");
+document.write("Привет, мир");
+
+const sum = 5 + 8;
+console.log("Результат операции: ", sum);
+
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_VALUE);
+
+const user={name: "Tom", age:24};
+console.log(user.name);
